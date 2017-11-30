4362c514d5f497607dde8a74e86f025ab5301e9e
Mike Callaghan <mikecallaghan67@gmail.com>
2017-11-30 18:02:29 -0500

    Add images and spin button

--- src/components/slot3d-item/slot3d-item.tsx
+++ src/components/slot3d-item/slot3d-item.tsx
@@ -1,29 +1,59 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Element, Prop } from '@stencil/core';
 
 @Component({
   tag: 'wr-slot3d-item',
   styleUrl: 'slot3d-item.scss'
 })
 export class Slot3dItem {
+  @Element() element: HTMLElement;
   @Prop() imageUrl: string;
   @Prop() name: string;
   @Prop() hasOverflow = false;
 
+  getNextSiblingCount(elem) {
+    var sibs = 0;
+    while (elem = elem.nextSibling) {
+      if (elem.nodeName === 'WR-SLOT3D-ITEM') { sibs++; }
+    }
+    return sibs;
+  }
+
+  getPreviousSiblingCount(elem: Node) {
+    var sibs = 0;
+    while (elem = elem.previousSibling) {
+      if (elem.nodeName === 'WR-SLOT3D-ITEM') { sibs++; }
+    }
+    return sibs;
+  }
+
   render() {
-    // Todo: Calculate item height!
+    const prevSiblings = this.getPreviousSiblingCount(this.element);
+    // const nextSiblings = this.getNextSiblingCount(this.element);
+    const panelSize = this.element.parentElement.clientHeight;
+    const itemCount = this.element.parentElement.childElementCount;
+    const itemAngle = 360 / itemCount;
+    const tz = Math.round((panelSize / 2) / Math.tan(Math.PI / itemCount));
+    const translateZ = `translateZ(${tz}px)`;
+    const angle = prevSiblings * itemAngle;
+    const rotate = `rotateX(${angle}deg)`;
+    const transform = `${rotate} ${translateZ}`;
+
     const styles = {
-      itemClass: 'text-item-wrap text-center mk-slot-item ' + this.hasOverflow ? 'mk-marqee' : '',
+      itemClass: 'text-item-wrap text-center mk-slot-item ' + (this.hasOverflow ? 'mk-marquee' : ''),
       itemStyle: {
-        'line-height': '1em'
+        'line-height': '1em',
+        transform: transform,
+        '-webkit-transform': transform
       },
-      imgStyle: {height: '38px'}
+      imgStyle: { height: panelSize + 'px' }
     };
 
+
     return (
-      <li class={styles.itemClass} style={styles.itemStyle}>
-        <img style={styles.imgStyle} src={this.imageUrl} /><br/>
+      <div class={styles.itemClass} style={styles.itemStyle}>
+        <img style={styles.imgStyle} src={this.imageUrl} /><br />
         <span class="mk-label">{this.name}</span>
-      </li>
+      </div>
     );
   }
 }
--- src/components/slot3d/slot3d.tsx
+++ src/components/slot3d/slot3d.tsx
@@ -6,27 +6,30 @@ import { Component, Element, Prop } from '@stencil/core';
 })
 export class Slot3d {
   @Element() element: HTMLElement;
-  
+
   @Prop() isRunning = false;
   @Prop() hasOverflow = false;
 
   render() {
     const rotateTransform = 'rotate(0deg)';
     const styles = {
-      spinnerClass: 'mk-spinner ' + this.isRunning ?  'mk-spinning' : '',
+      spinnerClass: 'mk-spinner ' + (this.isRunning ? 'mk-spinning' : ''),
       marqueeClass: this.hasOverflow ? 'mk-marqee' : '',
       spinnerStyle: {
         transform: rotateTransform,
         '-webkit-transform': rotateTransform
       }
     };
+    const numChildren = this.element.childElementCount;
 
     return (
-      <div>
+      <div class="mk-container">
+          {numChildren} children
         <div class="mk-slotContainer">
-          <ul class={styles.spinnerClass} style={styles.spinnerStyle}>
-          {this.element.children}
-          </ul>
+          {/* <div class={styles.spinnerClass} style={styles.spinnerStyle}> */}
+          <div class={styles.spinnerClass} style={styles.spinnerStyle}>
+            <slot />
+          </div>
         </div>
       </div>
     );
